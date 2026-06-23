057d30a0d9bbe85e95ab6e202d7dac58c706dfd3
thillagen <thillagen@gmail.com>
2026-06-23 22:19:12 +0800

    Update download URLs for Knotpad in Cask

--- Casks/knotpad.rb
+++ Casks/knotpad.rb
@@ -3,12 +3,12 @@ cask "knotpad" do
 
   on_intel do
     sha256 "636f38b4df3aa8098ec0c5b4b88ba9c8993f50c800d7016f31e85c322a5c9cc7"
-    url "https://github.com/knotpad/homebrew-knotpad/releases/download/v#{version}/knotpad_#{version}_x64.dmg"
+    url "https://github.com/knotpad/homebrew-tap/releases/download/v#{version}/knotpad_#{version}_x64.dmg"
   end
 
   on_arm do
     sha256 "d1d8d21717d14cd796c85a827cedd03ff2ad4e559f651519702fb2172c6c34b8"
-    url "https://github.com/knotpad/homebrew-knotpad/releases/download/v#{version}/knotpad_#{version}_aarch64.dmg"
+    url "https://github.com/knotpad/homebrew-tap/releases/download/v#{version}/knotpad_#{version}_aarch64.dmg"
   end
 
   name "Knotpad"
